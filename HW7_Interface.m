f2f146a311f8b39eb38957e12550fa3a36b5e061
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2019-11-03 23:50:13 -0600

    Add files via upload

---
+++ HW7_Interface.m
@@ -0,0 +1,24 @@
+clc,clear,close all
+
+button = 'y';
+
+while button == 'y' || button == 'Y'
+    % Type in dir to load image 
+    prompt = ('Please type in the image directory:\n');
+    dir = input(prompt,'s');
+    load(dir);
+    
+    if method == 1
+        run('/home/dewei/Desktop/HW2019F/AdvImg_HW/HW7_Polynomial.m')
+    else
+        run('/home/dewei/Desktop/HW2019F/AdvImg_HW/HW7_TPS.m')
+    end
+    
+    prompt = 'Do you want to try more?[y/n]:\n';
+    button = input(prompt,'s');
+    
+    if button == 'n' || button == 'N'
+        fprintf('Thanks\n');
+        break
+    end
+end
